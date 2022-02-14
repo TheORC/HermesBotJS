507054a5284f9d0dba41e17f1aac67b9bdd5808f
Oliver <oliverclarke50@gmail.com>
2022-02-14 11:12:39 +1030

    Started implemented database support.

---
+++ src/modules/database.js
@@ -0,0 +1,23 @@
+// Namespace dictats this is initizlized once.
+var mysqlPool;
+
+/***
+* Controlls the interface to the database.
+*/
+class DatabaseAdaptar {
+  constructor() {
+
+  }
+}
+
+/***
+* Handles connections too the database.
+*/
+class Pool {
+  constructor() {
+
+  }
+}
+
+module.exports.DatabaseAdaptar = DatabaseAdaptar;
+module.exports.Pool = Pool;
